c0fb6f4d1af7daf84a1c009fc86cc2bc17cec622
Vladimir <rybalkinv@inbox.ru>
2023-02-04 19:14:23 +0300

    дз2, готовый файл

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿int programm, num;
+Boolean begin = true;
+ 
+while (begin)
+{
+Console.WriteLine("------");
+Console.WriteLine("Введите число для соответствующей задачи или иное для выхода:");
+Console.WriteLine("1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.WriteLine("2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.WriteLine("3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+programm = Convert.ToInt32(Console.ReadLine());
+ 
+switch (programm)
+{
+case 1:
+//1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+ 
+Random random = new Random();
+int min = 100;
+int max = 999;
+int number = random.Next(min, max + 1);
+Console.WriteLine(number);
+int a = number / 10 % 10;
+Console.WriteLine(a);
+break;
+
+case 2:
+//2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет."
+int number1 = ReadInt("Введите число: ");
+int count = number1.ToString().Length;
+Console.Write(MakeArray(number1, count));
+ 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+ 
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+ 
+        result = (a / c) % 10;
+    }
+return result;
+}
+break;
+
+
+case 3:
+Console.Write("Введите день недели ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) {
+	if (Day >= 6) {
+		Console.Write("выходной");
+	} else {
+		Console.Write("будний день(((");
+	}
+} else {
+	Console.Write("нет такого дня ");
+}
+
+break;
+
+default:
+begin = false;
+break;
+}
+}
+
